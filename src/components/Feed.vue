b66e1be2accbe8aaa7d95bddff8062fef19408d7
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-10-12 23:33:59 +0900

    feat: 수정, 삭제 마크업

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,22 +1,16 @@
 <template>
     <Navbar/>
-    <div v-if="showAll">
-        <div class="searchbar input-group mt-5 mb-5">
-            <input @input="search" type="text" class="form-control" placeholder="키워드로 검색" aria-label="키워드로 검색" aria-describedby="basic-addon2">
-        </div>
-        <div class="feedCards">
-            <FeedCard v-for="feedData in allFeedData" :key="feedData" :feedData="feedData"/>
-        </div>
+    <div class="searchbar input-group mt-5 mb-5">
+        <input @input="search" type="text" class="form-control" placeholder="키워드로 검색" aria-label="키워드로 검색" aria-describedby="basic-addon2">
     </div>
-    <div v-else>
-        <FeedDetail :selectedFeed="selectedFeed" />
+    <div class="feedCards">
+        <FeedCard v-for="feedData in allFeedData" :key="feedData" :feedData="feedData"/>
     </div>
 </template>
 
 <script>
 import Navbar from "./Navbar.vue";
 import FeedCard from "./FeedCard.vue";
-import FeedDetail from "./FeedDetail.vue";
 import { ref, onMounted } from 'vue';
 import axios from 'axios';
 
@@ -46,13 +40,10 @@ export default {
     },
     components: {
         Navbar,
-        FeedCard,
-        FeedDetail
+        FeedCard
     },
     data() {
         return {
-            showAll: true,
-            selectedFeed: {},
         }
     },
 }
--- src/components/FeedCard.vue
+++ src/components/FeedCard.vue
@@ -1,19 +1,48 @@
 <template>
     <div class="card">
-        <img :src="feedData.thumbnailUrl" class="card-img-top" alt="...">
+        <img :src="feedData.thumbnailUrl" class="card-img-top">
         <div class="card-body">
             <h5 class="card-title">{{feedData.title}}</h5>
             <p class="card-text">{{content_shorten}}</p>
-            <button href="#" class="btn btn-primary">자세히 보기</button>
+            <button class="btn btn-warning" @click="showUpdateModal=true">수정하기</button>
             <button class="btn btn-danger" @click="showDeleteModal=true">삭제하기</button>
         </div>
+    </div>
+
+    <div class="updateModal" v-if="showUpdateModal">
+        <img class="mb-2" :src="feedData.thumbnailUrl" style="width: 200px; height: 200px;">
+        <input type="file" id="chooseFile" name="chooseFile" accept="image/*">
+        <h5 class="mb-4">author: {{feedData.author.nickname}}</h5>
+
+        <span class="input-group-text" id="basic-addon1">Title</span>
+        <input type="text" class="mb-2 form-control" :value="feedData.title" aria-label="Username" aria-describedby="basic-addon1">
+
+        <span class="input-group-text">Content</span>
+        <textarea class="mb-2 form-control" aria-label="With textarea" :value="feedData.content" style="height: 100px"></textarea>
+
+        <label class="input-group-text" for="inputGroupSelect01">Step</label>
+        <select class="mb-2 form-select" id="inputGroupSelect01">
+            <option selected>{{feedData.step}}</option>
+            <option value="PROGRESS">PROGRESS</option>
+            <option value="COMPLETE">COMPLETE</option>
+        </select>
+
+        <label class="input-group-text" for="inputGroupSelect01">Sos</label>
+        <select class="mb-5 form-select" id="inputGroupSelect01">
+            <option selected>{{feedData.sos}}</option>
+            <option value="PROGRESS">true</option>
+            <option value="COMPLETE">false</option>
+        </select>
+
+        <button class="btn btn-outline-dark" @click="sendUpdateToServer">예. 수정합니다.</button>
+        <button class="btn btn-outline-dark" @click="showUpdateModal=false">아니요. 취소합니다</button>
     </div>
 
     <div class="deleteModal" v-if="showDeleteModal">
         <h4>{{feedData.title}}</h4>
         <p>정말로 삭제하시겠습니까?</p>
-        <button class="btn btn-outline-secondary" @click="sendDeleteToServer">예. 삭제합니다.</button>
-        <button class="btn btn-outline-secondary" @click="showDeleteModal=false">아니요. 취소합니다</button>
+        <button class="btn btn-outline-dark" @click="sendDeleteToServer">예. 삭제합니다.</button>
+        <button class="btn btn-outline-dark" @click="showDeleteModal=false">아니요. 취소합니다</button>
     </div>
 </template>
 
@@ -26,13 +55,14 @@ export default {
     data() {
         return {
             content_shorten: this.shorten(this.feedData.content),
+            showUpdateModal: false,
             showDeleteModal: false,
         }
     },
     methods: {
         shorten(content) {
-            if (content.length > 50) {
-                return content.substring(0, 50) + "...";
+            if (content.length > 70) {
+                return content.substring(0, 70) + "...";
             }
             return content;
         }, 
@@ -56,6 +86,18 @@ export default {
     margin: 30px;
 }
 
+.updateModal {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 100;
+    border-radius: 10px;
+    padding: 40px;
+    width: 65%;
+    background-color: rgb(228, 183, 69);
+}
+
 .deleteModal {
     position: absolute;
     top: 50%;
@@ -65,6 +107,7 @@ export default {
     border-radius: 10px;
     padding: 40px;
     width: 500px;
-    background-color: lightgray;
+    color: white;
+    background-color:crimson;
 }
 </style>
